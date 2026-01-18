cbcc8b8dd149331a53971813063353df9f395704
jafar-ctcl <jafarctcl@gmai.com>
2026-01-18 23:58:25 +0530

    chore: dependency fixes and accessibility updates; pin ajv-keywords, fix CardDish anchor to button

--- src/Components/CardDish.js
+++ src/Components/CardDish.js
@@ -6,11 +6,10 @@ function CardDish(props) {
   return (
       
         <li>
-          <a href="#" className="card-link" onClick={(e)=>{e.preventDefault(); props.showPopupHandler(props.menuItem)}}>
-
-         <img src= {props.menuItem.strMealThumb} alt="" className='br-10' />
-          <h5>{props.menuItem.strMeal}</h5>
-          </a>
+          <button type="button" className="card-link" onClick={()=> props.showPopupHandler(props.menuItem)}>
+            <img src={props.menuItem.strMealThumb} alt={props.menuItem.strMeal} className='br-10' />
+            <h5>{props.menuItem.strMeal}</h5>
+          </button>
         </li>
       
   
